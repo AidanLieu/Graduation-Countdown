84e307f0d04cdf8d874ea24e21dee33673042706
AidanLieu <aidanlieu161@gmail.com>
2025-06-03 16:41:30 -0700

    Update main.js

--- main.js
+++ main.js
@@ -2,8 +2,7 @@ let visible = true;
 function clock(){
     const endDate = new Date("June 11 2025 00:00:00").getTime();
     const currentDate = new Date().getTime();
-    //const ms = endDate - currentDate;
-    const ms = 0
+    const ms = endDate - currentDate;
     var days = Math.floor(ms / (1000 * 60 * 60 * 24));
     var hours = Math.floor((ms % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
     var minutes = Math.floor((ms % (1000 * 60 * 60)) / (1000 * 60));
